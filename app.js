a78e674f8a14591bf0f450b2047697c3135ee3bb
sandipan-saha <sandipansaha.dev@outlook.com>
2024-02-22 14:27:21 +0530

    Update:
    - Added functions to:
    - - Save todo tasks to local storage, once they are added to DOM.
    - - Check if any prior todo tasks are saved in the local storage or not, and if saved, then display them in the DOM

--- app.js
+++ app.js
@@ -1,39 +1,38 @@
-// selectors
+// SELECTORS
 const todoInput = document.querySelector(".todo-input");
 const todoButton = document.querySelector(".todo-submit");
 const todoListWrapper = document.querySelector(".todo-list-wrapper");
 
-// event listeners
+// EVENT LISTENERS
+document.addEventListener("DOMContentLoaded", getToDos);
 todoButton.addEventListener("click", addToDo); // Event listener to add tasks
 todoListWrapper.addEventListener("click", deleteCheck); // Event listener to either mark tasks as completed or to delete tasks
-// functions
+
+// FUNCTIONS
+
+/* To take data data from input field and add to DOM */
 function addToDo(event) {
   // prevent form from trying submit data to backend
   event.preventDefault();
 
-  // To Do list <div>
-  const todoList = document.createElement("li"); // creating a new div
-  todoList.classList.add("todo-list"); // adding class name 'todoDiv'
-
   // To Do list <li>
-  const newToDo = document.createElement("span"); // creating a new li
+  const todoList = document.createElement("li"); // creating a new <li> row item
+  todoList.classList.add("todo-list"); // adding class name 'todo-list'
+
+  // To Do list <span>
+  const newToDo = document.createElement("span"); // creating a new <span>
   newToDo.classList.add("todo-item"); // adding class name 'todo-item'
   newToDo.innerText = todoInput.value;
 
-  // Adding the <li> to <div>
+  // Adding the 'todo-item' <span> to 'todo-list' <li> row
   todoList.appendChild(newToDo);
-
-  // // Creating a wrapper <div> for task action buttons
-  // const buttonsDiv = document.createElement("div");
-  // buttonsDiv.classList.add("btns-wrapper");
-  // todoDiv.appendChild(buttonsDiv);
 
   // Creating check-mark button
   const completedButton = document.createElement("button");
   completedButton.classList.add("complete-btn");
   completedButton.innerHTML = '<i class="fas fa-check"></i>';
 
-  // Adding the completed <button>> to buttonsDiv wrapper
+  // Adding the completed <button>> to 'todo-list' <li> row
   todoList.appendChild(completedButton);
 
   // Creating delete button
@@ -41,16 +40,20 @@ function addToDo(event) {
   deleteButton.classList.add("delete-btn");
   deleteButton.innerHTML = '<i class="fas fa-trash"></i>';
 
-  // Adding the delete <button>> to buttonsDiv wrapper
+  // Adding the delete <button>> to 'todo-list' <li> row
   todoList.appendChild(deleteButton);
 
-  // Adding the <div> to <ul> todo-list
+  // Adding the 'todo-list' <li> row to <ul> todo-list wrapper
   todoListWrapper.appendChild(todoList);
+
+  // Adding the todo task to local storage
+  saveLocalToDos(todoInput.value);
 
   // Clear input field value after adding data to list
   todoInput.value = "";
 }
 
+/* To mark tasks as completed or to delete tasks */
 function deleteCheck(event) {
   const item = event.target;
 
@@ -65,4 +68,61 @@ function deleteCheck(event) {
     const listItem = item.parentElement;
     listItem.classList.toggle("completed");
   }
+}
+
+/* To save tasks into local storage once added to DOM, so as not to loose them after refresh */
+function saveLocalToDos(task) {
+  let taskArray;
+  // checking if local storage has any data already
+  if (localStorage.getItem("Tasks:") === null) {
+    taskArray = []; // if nothing found, set task array to empty
+  } else {
+    taskArray = JSON.parse(localStorage.getItem("Tasks:")); // if something found, parse the data from local storage into task array
+  }
+  taskArray.push(task); // pushing the new task to taskArray
+  localStorage.setItem("Tasks:", JSON.stringify(taskArray)); // uploading the updated task array to local storage
+}
+
+/* To check if the local storage already has tasks and to display them in the DOM, even after refresh */
+function getToDos() {
+  let taskArray;
+  // checking if local storage has any data already
+  if (localStorage.getItem("Tasks:") === null) {
+    taskArray = []; // if nothing found, set task array to empty
+  } else {
+    taskArray = JSON.parse(localStorage.getItem("Tasks:")); // if something found, parse the data from local storage into task array
+  }
+  // Fetching existing data from local storage and displaying on screen
+  taskArray.forEach(function (task) {
+    // To Do list <li>
+    const todoList = document.createElement("li"); // creating a new <li> row item
+    todoList.classList.add("todo-list"); // adding class name 'todo-list'
+
+    // To Do list <span>
+    const newToDo = document.createElement("span"); // creating a new <span>
+    newToDo.classList.add("todo-item"); // adding class name 'todo-item'
+    newToDo.innerText = task;
+
+    // Adding the 'todo-item' <span> to 'todo-list' <li> row
+    todoList.appendChild(newToDo);
+
+    // Creating check-mark button
+    const completedButton = document.createElement("button");
+    completedButton.classList.add("complete-btn");
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+
+    // Adding the completed <button>> to 'todo-list' <li> row
+    todoList.appendChild(completedButton);
+
+    // Creating delete button
+    const deleteButton = document.createElement("button");
+    deleteButton.classList.add("delete-btn");
+    deleteButton.innerHTML = '<i class="fas fa-trash"></i>';
+
+    // Adding the delete <button>> to 'todo-list' <li> row
+    todoList.appendChild(deleteButton);
+
+    // Adding the 'todo-list' <li> row to <ul> todo-list wrapper
+    todoListWrapper.appendChild(todoList);
+  });
 }
